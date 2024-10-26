10ca77d0b9715acadffaafab72e093c54f719338
khomotso <kkgothamama@gmail.com>
2024-10-26 08:32:56 +0200

    done

--- fun.py
+++ fun.py
@@ -1,3 +1,6 @@
+from string import digits
+
+
 def dog_years():
     
     """
@@ -12,6 +15,26 @@ def dog_years():
     """
 
     #enter your code here
+def dog_years():
+    if human_years > 20:
+        human_years = 20
+
+    if human_years<= 2:
+        dog_years = human_years * 10.5
+    else:
+        dog_years = 21 + (human_years -2) * 4
+
+    return int(dog_years) 
+try:
+    human_years =float(input("input a dog age in human years"))
+    if human_years< 0:
+        print("age cannot be negative")
+    else:
+        print("the dog age in dog year is")    
+except ValueError:
+    print("please enter a vaild number.")
+
+
 
 def fizzbuzz(num):
     """
@@ -25,6 +48,23 @@ def fizzbuzz(num):
     """
 
     #enter your code here
+def Fizzbuzz(num):
+    result = []
+    for i in range(1,num + i): 
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+         result.append("Fizz")               
+        elif i % 5 == 0:
+            result.append("Buzz")
+    else:
+        result.append(str(1))
+
+    return "  ".join((15))  
+    print(Fizzbuzz(15))  
+        
+        
+
 
     
 
@@ -35,12 +75,18 @@ def word_lengths(sentence):
 
     Expected Output:
     ```
-    Input a sentence: "Aunty Yankho is amazing"
-    Output: {'Aunty': 5, 'Yankho': 6, 'is': 2, 'amazing': 7}
+    Input a sentence: "Aunty Yankho is amazi"
+    Output: {'Aunty': 5, 'ngYankho': 6, 'is': 2, 'amazing': 7}
     ```
     """
     
     #enter your code here
+def word_lengths(sentence):
+    words = sentence.split(" ")
+    word_lengths_dict ={word: len(word)for word in words} 
+    return word_lengths_dict 
+
+word_lengths("Aunty Yankho is amazi")
 
 def cube_sum(number):
     """
@@ -53,3 +99,8 @@ def cube_sum(number):
     """
     
     #enter your code here
+def cube_sum(numbers):
+    digits = [int(digits) for digit in str(num)]
+    cube_sum =sum(digits ** 3 for digits in digits)
+
+print(cube_sum(123))   
